45f3a819f47a23fb57ab9b2fd0a3d58bd71b3377
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-08-30 20:35:18 +0700

    update isi dari file materi string

--- Kotlin Fundamental/Data Type/String/src/App.kt
+++ Kotlin Fundamental/Data Type/String/src/App.kt
@@ -3,5 +3,17 @@ fun main() {
     val text  = "Kotlin"
     val firstChar = text[0]
 
-    print("First character of $text is $firstChar")
+    println("First character of $text is $firstChar")
+//    ambil tiap karakter pada string
+    for (char in text){
+        println("$char ")
+    }
+//    escaped string
+    println("nama saya \"GUNA DERMAWAN\" \u00A9")
+//    raw string
+    println("""
+        nama saya guna dermawawan,
+        saya suka coding, tpi kadang jga lupa
+        dokumentasinya :D
+    """.trimIndent())
 }
--- Membangun dan Menjalankan Program Kotlin/Latihan/Latihan 1/src/Task.kt
+++ Membangun dan Menjalankan Program Kotlin/Latihan/Latihan 1/src/Task.kt
@@ -1,3 +1,4 @@
+
 fun main() {
     println("Kotlin is Awesome!")
 }
